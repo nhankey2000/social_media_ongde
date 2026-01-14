c9a51d270b5b094c094db738bcbadd79d691461c
nhankey2000 <thanhnhan01216851830@gmail.com>
2026-01-14 09:30:54 +0700

    Update ignore file

--- routes/api.php
+++
@@ -1,5 +0,0 @@
-<?php
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Webhooks\DriveWebhookController;
-
-Route::post('/webhook/download-drive', [DriveWebhookController::class, 'download']);
